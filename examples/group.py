58288c058cd9a898a39295afd8c7bb5deec0904a
Max <maxexmachina@gmail.com>
2021-04-04 15:57:48 +0300

    collision check added, still debugging

--- examples/group.py
+++ examples/group.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 from mavros_msgs.srv import SetMode, CommandBool, CommandVtolTransition, CommandHome
 
 from munkres import Munkres, print_matrix 
+from sympy import Point3D, Line3D, Ray3D, Segment3D
 
 instances_num = 6 #количество аппаратов
 freq = 20 #Герц, частота посылки управляющих команд аппарату
@@ -29,6 +30,188 @@ formation_string = "string"
 formation = []
 formation_global = []
 
+class Vector3D:
+  def __init__(self, direction):
+    self.x_limits = [-12, 12]
+    self.y_limits = [-12, 12]
+    self.z_limits = [-1, 3]
+    max_dir = max(direction)
+    direction_normalized = [i/max_dir for i in direction]
+    res = direction_normalized.copy()
+    limits = []
+    if (res[0] >= 0):
+      limits.append(self.x_limits[1])
+    else:
+      limits.append(self.x_limits[0])
+    
+    if (res[1] >= 0):
+      limits.append(self.y_limits[1])
+    else:
+      limits.append(self.y_limits[0])
+
+    if (res[2] >= 0):
+      limits.append(self.y_limits[1])
+    else:
+      limits.append(self.y_limits[0])
+
+    index_max = direction.index(max_dir)
+    lim_max = limits[index_max]
+
+    for i in range(len(direction_normalized)):
+      res[i] = lim_max * direction_normalized[i]
+    for i in range(len(direction_normalized)):
+      if (np.abs(res[i]) > np.abs(limits[i])):
+        koef = limits[i]/res[i]
+        res = [koef*j for j in res]
+
+    self.x = res[0]
+    self.y = res[1]
+    self.z = res[2]
+
+  def length(self):
+    return (self.x**2 + self.y**2 + self.z**2)**0.5
+  def __eq__(self, other):
+    return (self.x == other.x and self.y == other.y and self.z == other.z)
+  def __neg__(self):
+    return Vector3D(-self.x, -self.y, -self.z)
+  def __add__(self, other):
+    p = [self.x + other.x, self.y + other.y, self.z + other.z]
+    return Vector3D(p)
+
+class Drone:
+  def __init__(self, position, size=0.9):
+    self.position = position #Стартовая позиция
+    #self.velocity = Vector3([]) # Вектрор скорости
+    self.velocity_mod = 0 #Скорость
+    self.size = size #Размер
+    #self.setTarget(position)
+
+  def setTarget(self, target):
+    #Функция задания целевой точки
+    self.target = target
+    self.line = Line3D(self.position, self.target) #Прямая, на которой лежит траектория
+    self.ray = Ray3D(self.position, self.target) #Луч, идущий из стартовой позиции в направлении цели
+    self.segment = Segment3D(self.position, self.target) #Отрезок, соединяющий стартовую позицию с целевой
+    self.start_delay = 0 #Задержка старта
+
+    direction = [self.target.x - self.position.x,self.target.y - self.position.y, self.target.z - self.position.z ]
+    self.velocity = Vector3(direction)
+    self.velocity_mod = self.velocity.length()
+
+  def countDistanceBetweenParallel(self, l1, l2): 
+    #Функция, которая расчитывает расстояние между параллельными прямыми
+    return l1.distance(l2.random_point())
+  def countDistanceBetweenSkew(self, l1, l2):
+    #Функция, которая расчитывает расстояние между непараллельными скрещивающимися прямыми
+    inter_point = l1.projection(l2)
+    l = l2.parallel_line(inter_point)
+    return self.countDistanceBetweenParallel(l1, l2)
+
+  def checkCollision(self, other, safety_factor=1.5):
+    #Функция, которая проверяет, столкнутся ли два дрона и вносит задержки по времени
+    #TODO: Нет учета разницы предельных скоростей по направлениям
+    safe = False
+    linesInter = self.line.intersection(other.line) #Ищем пересечение прямых
+    if (type(linesInter) == type(self.line)):
+      #Случай совпадающих прямых
+
+      total_delay = self.start_delay - other.start_delay
+      self_is_first = not self.ray.contains(other.position)
+      #Если self летит впереди
+      if self_is_first:
+        total_delay = -total_delay
+      #Вычисляем расстояние между дронами с учетом их размеров
+      dist = self.position.distance(other.position) - self.size/2 - other.size/2
+      #Считаем разницу во времени, с которой дроны будут проходить через одни и те же точки
+      t = dist/np.abs(self.velocity_mod - other.velocity_mod) + total_delay
+      # Если разница во времени отрицательная, добавляем задержку дрону, который летит сзади
+      if (t >= 0 ):
+        safe = True
+      elif self_is_first:
+        other.start_delay += np.abs(t)
+        safe = False
+      else:
+        safe = False
+        self.start_delay += np.abs(t)
+      return safe
+
+    elif (linesInter): 
+      #Случай пересекающихся прямых
+      dist1 = self.position.distance(linesInter)
+      dist2 = other.position.distance(linesInter)
+      #Рассчитываем время, через которое дроны прибудут в точку пересечения
+      t1 = dist1/self.velocity_mod + self.start_delay
+      t2 = dist2/other.velocity_mod + other.start_delay
+
+      safe_is_first = (t1 <= t2)
+      #Рассчитываем временные "опасные зоны"
+      delta_t1 = (self.size/2)/self.velocity_mod*safety_factor
+      delta_t2 = (other.size/2)/other.velocity_mod*safety_factor
+      danger_bounds1 = (t1 - delta_t1, t1 + delta_t2)
+      danger_bounds2 = (t2 - delta_t1, t2 + delta_t1)
+
+      if (danger_bounds2[1] <= danger_bounds1[0]) or (danger_bounds1[1] <= danger_bounds1[0]):
+        safe = True
+        return safe
+      elif (not safe_is_first):
+        self.start_delay += (danger_bounds2[1] - danger_bounds1[0])*1.1
+        safe = False
+        return safe
+      else:
+        other.start_delay += (danger_bounds1[1] - danger_bounds0[0])*1.1
+        safe = False
+        return safe
+
+    else: 
+      #Случай скрещивающихся прямых
+
+      #Проверяем на параллельность и рассчитывыем расстояние между прямыми
+      isParallel = self.line.is_parallel(other.line)
+      
+      if isParallel:
+        dist = self.countDistanceBetweenParallel(self.line, other.line)
+      else:
+        dist = self.countDistanceBetweenSkew(self.line, other.line)
+      
+      #Если оно больше суммы радиусов дронов, то пролет безопасен
+      if (dist > self.size /2 + other.size/2):
+        safe = True
+        return safe
+      else:
+        #Случай параллельных прямых
+        if isParallel and (self.velocity == -other.velocity):
+          #TODO:НЕ УЧТЕНО В ДРУГИХ СЛУЧАЯХ
+          t1 = self.target.copy()
+          t2 = other.target.copy()
+          self.setTarget(t2)
+          other.setTarget(t1)
+          safe = False
+          return safe
+        elif not isParallel:
+          #Случай скрещенных непараллельных прямых
+          
+          #Рассчитываем точки пересечения с проекциями
+          p1 = self.line.projection(other.line)
+          p2 = other.line.projection(self.line)
+
+          #Рассчитываем время, через которое дрон прибудет в точку пересечения
+          t1 = dist1/self.velocity_mod + self.start_delay
+          t2 = dist2/other.velocity_mod + other.start_delay
+
+          #Рассчитываем временные "опасные зоны"
+          delta_t1 = (self.size/2)/self.velocity_mod*safety_factor
+          delta_t2 = (other.size/2)/other.velocity_mod*safety_factor
+          danger_bounds1 = (t1 - delta_t1, t1 + delta_t2)
+          danger_bounds2 = (t2 - delta_t1, t2 + delta_t1)
+
+          if (danger_bounds2[1] <= danger_bounds1[0]) or (danger_bounds1[1] <= danger_bounds1[0]):
+            safe = True
+            return safe
+          else:
+            self.start_delay += (danger_bounds2[1] - danger_bounds1[0])*1.1
+            safe = False
+            return safe
+
 
 class Drones:
   def __init__(self, data):
@@ -36,16 +219,27 @@ class Drones:
     self.exist = True
     self.positions = []
     self.targets = []
+    self.drones = []
     try:
       self.numberOfDrones = len(data.keys()) 
+      
       for i in range(self.numberOfDrones):
         self.positions.append(data[i+1]['local_position/pose'].pose.position)
+        #print(data[i+1]['local_position/velocity_local'].twist.linear)
+        #print(positions[i])
+        
       self.matrix = np.zeros((6, 6))
       
     except Exception:
       print('exception in drones init')
       self.exist = False
       # print('positions:\n', self.positions)
+    
+    if (len(self.positions)>0):
+      for i in range(self.numberOfDrones):
+        pos = self.positions[i]
+        #print([pos.x, pos.y, pos.z])
+        self.drones.append(Drone(Point3D([pos.x, pos.y, pos.z])))
   
   def processMatrix(self):
     
@@ -64,7 +258,7 @@ class Drones:
     
     for i in range(6):
       for j in range(6):
-        #enough to calculate upper half 
+        #TODO:enough to calculate upper half 
         self.matrix[i][j] = self.find_distance(self.positions[i], formation_global[j])
 
     
@@ -78,6 +272,7 @@ class Drones:
    
     
   def find_distance(self, p1, p2):
+    #TODO: Добавить учет разницы скоростей по направлениям
     #print('find dist')
     if (type(p2) == type([])):
       p2_p = Point(p2[0], p2[1], p2[2])
@@ -242,7 +437,7 @@ def mc_example(pt, n, dt):
 
   if dt>10:
     coor = formation_global[drones_global.targets[n - 1 ]]
-    print(coor)
+    #print(coor)
     set_pos(pt, coor[0], coor[1], coor[2])
 
   # #летим в одном направлении, разносим по высоте
